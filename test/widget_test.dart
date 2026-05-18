c9ea512e15a2e2d61f651a0c509beb2aa35dac00
JMNaveeth <naveethkinniya2001@gmail.com>
2026-05-18 12:28:23 +0530

    Fix widget test to use correct app class name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,7 +14,7 @@ import 'package:luxe_salon_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const LuxeApp());
+    await tester.pumpWidget(const LuxeSalonApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
